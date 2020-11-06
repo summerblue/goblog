24c93ae1f5b190cfd38717ad81ae74ee98420b67
Summer <summer@learnku.com>
2020-11-06 19:24:22 +0800

    切割模板

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -8,6 +8,7 @@ import (
 	"goblog/pkg/types"
 	"html/template"
 	"net/http"
+	"path/filepath"
 	"strconv"
 	"unicode/utf8"
 
@@ -64,12 +65,24 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "500 服务器内部错误")
 	} else {
-		// 2. 加载模板
-		tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+		// ---  2. 加载模板 ---
+
+		// 2.0 设置模板相对路径
+		viewDir := "resources/views"
+
+		// 2.1 所有布局模板文件 Slice
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
 		logger.LogError(err)
 
-		// 3. 渲染模板，将所有文章的数据传输进去
-		tmpl.Execute(w, articles)
+		// 2.2 在 Slice 里新增我们的目标文件
+		newFiles := append(files, viewDir+"/articles/index.gohtml")
+
+		// 2.3 解析模板文件
+		tmpl, err := template.ParseFiles(newFiles...)
+		logger.LogError(err)
+
+		// 2.4 渲染模板，将所有文章的数据传输进去
+		tmpl.ExecuteTemplate(w, "app", articles)
 	}
 }
 
