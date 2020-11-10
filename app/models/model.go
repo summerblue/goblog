9458cb3e5691f5f556b6ea2f17f1998f70e4aa4b
Summer <summer@learnku.com>
2020-11-10 14:23:56 +0800

    创建用户

--- app/models/model.go
+++ app/models/model.go
@@ -2,11 +2,18 @@ package models
 
 import (
 	"goblog/pkg/types"
+	"time"
 )
 
 // BaseModel 模型基类
 type BaseModel struct {
-	ID uint64
+	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;not null"`
+
+	CreatedAt time.Time `gorm:"column:created_at;index"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index"`
+
+	// 支持 gorm 软删除
+	// DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" sql:"index"`
 }
 
 // GetStringID 获取 ID 的字符串格式
