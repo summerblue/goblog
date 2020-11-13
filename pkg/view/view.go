8cb528ddd54c27fabfec57a1f49d59cd2f04c37f
Summer <summer@learnku.com>
2020-11-13 16:10:40 +0800

    Flash 消息提示

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"goblog/pkg/auth"
+	"goblog/pkg/flash"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"html/template"
@@ -29,6 +30,7 @@ func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
 	// 1. 通用模板数据
 	data["isLogined"] = auth.Check()
 	data["loginUser"] = auth.User
+	data["flash"] = flash.All()
 
 	// 2. 生成模板文件
 	allFiles := getTemplateFiles(tplFiles...)
