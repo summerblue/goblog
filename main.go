f8462fae24d278c064df5e5c40a00241d994340e
Summer <summer@learnku.com>
2021-11-01 21:46:33 +0800

    重构 Int64ToString

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/summerblue/goblog/pkg/logger"
 	"github.com/summerblue/goblog/pkg/route"
+	"github.com/summerblue/goblog/pkg/types"
 )
 
 var router = mux.NewRouter()
@@ -78,7 +79,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL": route.Name2URL,
-				"Int64ToString": Int64ToString,
+				"Int64ToString": types.Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogError(err)
@@ -441,11 +442,6 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
-}
-
-// Int64ToString 将 int64 转换为 string
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
 }
 
 // Delete 方法用以从数据库中删除单条记录
