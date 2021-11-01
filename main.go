0b6bc48edff2d961e99f0cf76c827b2099752bff
Summer <summer@learnku.com>
2021-11-01 17:49:48 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,8 +6,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
