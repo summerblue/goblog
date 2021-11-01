12cc39514845d81b7f3f0ae69d350db2de6a833a
Summer <summer@learnku.com>
2021-11-01 18:25:24 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 	} else if r.URL.Path == "/about" {
