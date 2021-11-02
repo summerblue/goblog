12b05b85c30abf8358380c3ab0456bb017dc26eb
Summer <summer@learnku.com>
2021-11-02 20:51:46 +0800

    用户的文章

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"goblog/app/models"
 	"goblog/pkg/password"
+	"goblog/pkg/route"
 )
 
 // User 用户模型
@@ -24,5 +25,5 @@ func (user *User) ComparePassword(_password string) bool {
 
 // Link 方法用来生成用户链接
 func (user *User) Link() string {
-	return ""
+	return route.Name2URL("users.show", "id", user.GetStringID())
 }
