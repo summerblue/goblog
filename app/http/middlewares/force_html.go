5c00e554c75e5991b6e272bc7c848e13a7088939
Summer <summer@learnku.com>
2021-11-02 13:11:40 +0800

    重构中间件

---
+++ app/http/middlewares/force_html.go
@@ -0,0 +1,13 @@
+package middlewares
+
+import "net/http"
+
+// ForceHTML 强制标头返回 HTML 内容类型
+func ForceHTML(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
