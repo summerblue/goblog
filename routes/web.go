8cae2ef168ec5e911f04f897ee021bffaccda4e7
Summer <summer@learnku.com>
2022-08-17 10:04:15 +0800

    美化文章列表页

--- routes/web.go
+++ routes/web.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
-	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -27,6 +26,10 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
+	// 静态资源
+	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
+
 	// 中间件：强制内容类型为 HTML
-	r.Use(middlewares.ForceHTML)
+	// r.Use(middlewares.ForceHTML)
 }
