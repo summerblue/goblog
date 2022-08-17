8b1e2e2ca82550d82357379976437ec0abbee632
Summer <summer@learnku.com>
2022-08-17 15:29:26 +0800

    会话和认证管理

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -15,3 +15,8 @@ type User struct {
 	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
 	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
+
+// ComparePassword 对比密码是否匹配
+func (user *User) ComparePassword(password string) bool {
+	return user.Password == password
+}
