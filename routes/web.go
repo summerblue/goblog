8f819fbfe03b9dede8dafe0f2300ba4e81184a1e
Summer <summer@learnku.com>
2022-08-17 09:45:03 +0800

    重构中间件

--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -25,4 +26,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/articles/{id:[0-9]+}/edit", ac.Edit).Methods("GET").Name("articles.edit")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
+
+	// 中间件：强制内容类型为 HTML
+	r.Use(middlewares.ForceHTML)
 }
