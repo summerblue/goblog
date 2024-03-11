ff774348ed27d1e48c740f902b8086de058e2424
Summer <summer@learnku.com>
2024-03-11 20:10:50 +0800

    embed 打包 CSS/JS 静态文件

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ import (
 //go:embed resources/views/layouts/*
 var tplFS embed.FS
 
+//go:embed public/*
+var staticFS embed.FS
+
 func init() {
 	// 初始化配置信息
 	config.Initialize()
@@ -25,11 +28,11 @@ func main() {
 	// 初始化 SQL
 	bootstrap.SetupDB()
 
-	// 初始化模板
+	// 初始化模板加载
 	bootstrap.SetupTemplate(tplFS)
 
 	// 初始化路由绑定
-	router := bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute(staticFS)
 
 	err := http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 	logger.LogError(err)
