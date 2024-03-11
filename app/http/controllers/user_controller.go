c987036e5d82eefb41f296310f942e98d3f71177
Summer <summer@learnku.com>
2024-03-11 19:46:19 +0800

    用户的文章

---
+++ app/http/controllers/user_controller.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"fmt"
+	"goblog/app/models/article"
+	"goblog/app/models/user"
+	"goblog/pkg/logger"
+	"goblog/pkg/route"
+	"goblog/pkg/view"
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+// UserController 用户控制器
+type UserController struct {
+}
+
+// Show 用户个人页面
+func (*UserController) Show(w http.ResponseWriter, r *http.Request) {
+
+	// 1. 获取 URL 参数
+	id := route.GetRouteVariable("id", r)
+
+	// 2. 读取对应的文章数据
+	_user, err := user.Get(id)
+
+	// 3. 如果出现错误
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			// 3.1 数据未找到
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 用户未找到")
+		} else {
+			// 3.2 数据库错误
+			logger.LogError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		// ---  4. 读取成功，显示用户文章列表 ---
+		articles, err := article.GetByUserID(_user.GetStringID())
+		if err != nil {
+			logger.LogError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		} else {
+			view.Render(w, view.D{
+				"Articles": articles,
+			}, "articles.index", "articles._article_meta")
+		}
+	}
+}
--- app/models/user/user.go
+++ app/models/user/user.go
@@ -4,6 +4,7 @@ package user
 import (
 	"goblog/app/models"
 	"goblog/pkg/password"
+	"goblog/pkg/route"
 )
 
 // User 用户模型
@@ -24,6 +25,6 @@ func (user *User) ComparePassword(_password string) bool {
 }
 
 // Link 方法用来生成用户链接
-func (user *User) Link() string {
-	return ""
+func (user User) Link() string {
+	return route.Name2URL("users.show", "id", user.GetStringID())
 }
