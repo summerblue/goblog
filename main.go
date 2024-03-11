b70f65b77c81e4e6ddb8701c374fc4e86b3f1e10
Summer <summer@learnku.com>
2024-03-11 09:00:07 +0800

    重构 RouteName2URL

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"goblog/pkg/route"
 	"html/template"
 	"log"
 	"net/http"
@@ -74,7 +75,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		// 4. 读取成功，显示文章
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
-				"RouteName2URL": RouteName2URL,
+				"RouteName2URL": route.Name2URL,
 				"Int64ToString": Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.gohtml")
@@ -510,6 +511,9 @@ func (a Article) Delete() (rowsAffected int64, err error) {
 func main() {
 	initDB()
 	createTables()
+
+	route.Initialize()
+	router = route.Router
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
---
+++ pkg/route/router.go
@@ -0,0 +1,23 @@
+// Package route 路由相关
+package route
+
+import "github.com/gorilla/mux"
+
+// Router 路由对象
+var Router *mux.Router
+
+// Initialize 初始化路由
+func Initialize() {
+	Router = mux.NewRouter()
+}
+
+// Name2URL 通过路由名称来获取 URL
+func Name2URL(routeName string, pairs ...string) string {
+	url, err := Router.Get(routeName).URL(pairs...)
+	if err != nil {
+		// checkError(err)
+		return ""
+	}
+
+	return url.String()
+}
