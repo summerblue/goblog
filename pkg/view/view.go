5d5c132d280b81b25d28d89793d990a074265965
Summer <summer@learnku.com>
2024-03-11 17:38:39 +0800

    注册页面

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -10,8 +10,21 @@ import (
 	"strings"
 )
 
-// Render 渲染视图
+// D 是 map[string]interface{} 的简写
+type D map[string]interface{}
+
+// Render 渲染通用视图
 func Render(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+// RenderSimple 渲染简单的视图
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+// RenderTemplate 渲染视图
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	// 1 设置模板相对路径
 	viewDir := "resources/views/"
 
@@ -35,6 +48,6 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	logger.LogError(err)
 
 	// 6 渲染模板
-	err = tmpl.ExecuteTemplate(w, "app", data)
+	err = tmpl.ExecuteTemplate(w, name, data)
 	logger.LogError(err)
 }
